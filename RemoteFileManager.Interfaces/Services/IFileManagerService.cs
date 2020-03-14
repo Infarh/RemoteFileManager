59391d08a30037a0cf370c140fc96c496aea84e5
Infarh <shmachilin@yandex.ru>
2020-03-14 12:17:11 +0300

    Интерфейс сервиса

---
+++ RemoteFileManager.Interfaces/Services/IFileManagerService.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace RemoteFileManager.Interfaces.Services
+{
+    public interface IFileManagerService
+    {
+        IEnumerable<DriveInfo> GetRoots();
+
+        IEnumerable<DirectoryInfo> GetDirectories(string Path);
+
+        IEnumerable<FileInfo> GetFiles(string Path);
+
+        FileInfo GetFileInfo(string Path);
+
+        DirectoryInfo GetDirectoryInfo(string Path);
+
+        void CopyFile(string Source, string Destination);
+
+        void MoveFile(string Source, string Destination);
+
+        void DeleteFile(string Path);
+
+        DirectoryInfo CreateDirectory(string Path);
+
+        void CopyDirectory(string Source, string Destination);
+
+        void MoveDirectory(string Source, string Destination);
+
+        void DeleteDirectory(string Path);
+
+        int Execute(string Path);
+    }
+}
