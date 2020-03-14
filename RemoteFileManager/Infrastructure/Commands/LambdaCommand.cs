e9b4740ea7ee22240b4dfb2b52117abedc1148be
Infarh <shmachilin@yandex.ru>
2020-03-14 11:51:28 +0300

    Базовая инфраструктура MVVM

---
+++ RemoteFileManager/Infrastructure/Commands/LambdaCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using RemoteFileManager.Infrastructure.Commands.Base;
+
+namespace RemoteFileManager.Infrastructure.Commands
+{
+    internal class LambdaCommand : Command
+    {
+        private readonly Action<object> _Execute;
+        private readonly Func<object, bool> _CanExecute;
+
+        public LambdaCommand(Action Execute, Func<bool> CanExecute = null)
+            : this(p => Execute(), CanExecute is null ? null : new Func<object, bool>(p => CanExecute()))
+        {
+            
+        }
+
+        public LambdaCommand(Action<object> Execute, Func<object, bool> CanExecute = null)
+        {
+            _Execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
+            _CanExecute = CanExecute;
+        }
+
+        public override bool CanExecute(object parameter) => _CanExecute?.Invoke(parameter) ?? true;
+
+        public override void Execute(object parameter) => _Execute(parameter);
+    }
+}
---
+++ RemoteFileManager/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using RemoteFileManager.Annotations;
+
+namespace RemoteFileManager.ViewModels.Base
+{
+    internal class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string PropertyName = null) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(PropertyName);
+            return true;
+        }
+    }
+}
