9c79548733330b7d564c5ae1ba87e7bdbfc8c970
Infarh <shmachilin@yandex.ru>
2020-03-14 12:04:27 +0300

    Инфраструктура сервера в консоли

---
+++ RemoteFileManager.ConsoleServer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RemoteFileManager.ConsoleServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Нажмите Enter для выхода...");
+            Console.ReadLine();
+        }
+    }
+}
